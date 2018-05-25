3118fc276bc28be00897860d6b5a5f500866f5c2
mm2ra <39447881+mm2ra@users.noreply.github.com>
2018-05-25 14:52:04 -0400

    Add files via upload

--- YelpAPI.js
+++ YelpAPI.js
@@ -4,37 +4,36 @@ import "./App.css";
 import axios from "axios";
 
 export default class YelpAPI extends React.Component {
- constructor(props) {
-   super(props);
-   this.state = {
-     namecost: []
-   };
+  constructor(props) {
+    super(props);
+    this.state = {
+      namecost: []
+    };
   }
 
- componentDidMount() {
-   axios.get("./response.json").then(res => {
-     const data = res.data
-     const names = data.businesses.map(restaurant => restaurant.name);
-     const price = data.businesses.map(restaurant => restaurant.price);
-     const namecost = [];
-     for (let i = 0; i < names.length; i++) {
-      namecost.push({ names: names[i], price: price[i] });
-    }
-    this.setState({ namecost: namecost });
-  });
-}
-
- render() {
-   let output = []
-    for(let i = 0; i < this.state.namecost.length; i++){
-      if (this.state.namecost[i].price == "$") {
-        output.push(this.state.namecost[i]);
+  componentDidMount() {
+    axios.get("./response.json").then(res => {
+      const data = res.data;
+      const names = data.businesses.map(restaurant => restaurant.name);
+      const price = data.businesses.map(restaurant => restaurant.price);
+      const namecost = [];
+      for (let i = 0; i < names.length; i++) {
+        namecost.push({ names: names[i], price: price[i] });
       }
-    }
-    console.log(output); 
-  return (
+      this.setState({ namecost: namecost });
+    });
+  }
 
-    <output/>
-  );
- }
+  render() {
+    let output = [];
+    if (this.props.isClicked) {
+      for (let i = 0; i < this.state.namecost.length; i++) {
+        if (this.state.namecost[i].price == "$") {
+          output.push(this.state.namecost[i]);
+        }
+      }
+      console.log(output);
+    }
+    return <output />;
+  }
 }
