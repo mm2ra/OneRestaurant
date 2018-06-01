ea54d7aa56cbc3dbe6ab0b7d58d5dd8977d466e0
Manana Metreveli <mananametreveli@mananas-air.lan>
2018-06-01 11:47:47 -0400

    Final version of App

--- src/MileForm.js
+++ src/MileForm.js
@@ -3,12 +3,14 @@ import "./App.css";
 import SelectField from "material-ui/SelectField";
 import { createMuiTheme } from '@material-ui/core/styles';
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+
 import Button from "@material-ui/core/Button";
 import * as criteriaArray from "./criteriaArray";
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import CardActions from '@material-ui/core/CardActions';
+
 
 export default class MileForm extends React.Component {
   constructor(props) {
@@ -82,14 +84,13 @@ export default class MileForm extends React.Component {
       if (this.props.output.length == 0) {
         return (
           <div>
-            {" "}
             No restaurants returned your search! Please refresh and try
-            different criteria.{" "}
+            different criteria.
           </div>
         );
       }
       return (
-        <div className= "Restaurant">
+        <div>
         <Card>
         <CardContent>
           <Typography color="darkslategrey">
@@ -106,7 +107,7 @@ export default class MileForm extends React.Component {
           </Button>
         </CardActions>
         </Card>
-      </div>
+        </div>
       );
     } else if (
       this.props.index == criteriaArray.criteria.length - 1 &&
@@ -115,30 +116,29 @@ export default class MileForm extends React.Component {
       if (this.props.output.length == 0) {
         return (
           <div>
-            {" "}
             No restaurants returned your search! Please refresh and try
-            different criteria.{" "}
+            different criteria.
           </div>
         );
       }
       return (
-         <div className= "Restaurant">
-          <Card>
-          <CardContent>
-            <Typography color="darkslategrey">
-              OneRestaurant
-            </Typography>
-            <Typography variant="headline" component="h2">
-              {myRest.name}
-            </Typography>
-          </CardContent>
-          <img src={myRest.image} width="400" height="341"/>
-          <CardActions className= "Link"> 
-            <Button style={{backgroundColor: "#A6BDB1"}} size="small" variant="outlined">
-              <a href={myRest.url}>See on Yelp</a> 
-            </Button>
-          </CardActions>
-          </Card>
+        <div>
+        <Card>
+        <CardContent>
+          <Typography color="darkslategrey">
+            OneRestaurant
+          </Typography>
+          <Typography variant="headline" component="h2">
+            {myRest.name}
+          </Typography>
+        </CardContent>
+        <img src={myRest.image} width="400" height="341"/>
+        <CardActions className= "Link"> 
+          <Button style={{backgroundColor: "#A6BDB1"}} size="small" variant="outlined">
+            <a href={myRest.url}>See on Yelp</a> 
+          </Button>
+        </CardActions>
+        </Card>
         </div>
       );
     }
